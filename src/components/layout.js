f7600ea14b711725ceb64434c4e65121ce5b0120
hzzheng <zhchaozju@gmail.com>
2019-02-13 16:01:00 +0800

    chore: add some layout style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,20 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 
 import cls from './layout.module.scss'
 
 export default ({ children }) => (
-  <div className={cls.app}>
+  <div className={cls.container}>
     <Helmet>
       <title>CNOTE</title>
     </Helmet>
-    <header>
-      <h1>
-        <Link to="/" className={cls.logo}>
-          CNOTE
-        </Link>
-      </h1>
-      <ul className={cls.nav}>
-        {/* <li>
-          <Link to="/" exact {...active}>
-            HOME
-          </Link>
-        </li> */}
-        {/* <li>
-          <Link to="/contact" {...active}>
-            Contact
-          </Link>
-        </li> */}
-      </ul>
-    </header>
-    {children}
+    <section className={cls.content}>
+      {children}
+    </section>
+    <aside>
+      <section>
+        hello
+      </section>
+    </aside>
   </div>
 )
