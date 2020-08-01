8a22478d7fe585b1f1c0865f9b73e0c2a22f9603
hzzheng <zhchaozju@gmail.com>
2020-08-01 09:11:43 +0800

    chore: update header font

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -24,6 +24,7 @@ export default ({ children, data }) => (
         <title>CNOTE</title>
         <link rel="shortcut icon" href="https://blog-1258648987.cos.ap-shanghai.myqcloud.com/blog/favicon.ico" type="image/x-icon" />
         <link rel="stylesheet" href="https://blog-1258648987.cos.ap-shanghai.myqcloud.com/blog/iconfont.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet" />
         <script src="//cdn1.lncld.net/static/js/3.0.4/av-min.js" />
         <script src="//unpkg.com/valine/dist/Valine.min.js" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
